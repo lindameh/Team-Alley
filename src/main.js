e33295ab8459dc94f144373268818472bcf7bc30
lindameh <huang.menglin@dhs.sg>
2020-10-26 20:33:51 +0800

    build signup and login function

--- src/main.js
+++ src/main.js
@@ -16,11 +16,24 @@
  */
 import Vue from 'vue';
 import App from './App.vue';
-// You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
 import router from './starterRouter';
 import NowUiKit from './plugins/now-ui-kit';
+import * as firebase from "firebase";
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnivbRInwjLMGThsUEX_Xqh2X4g1pXb90",
+  authDomain: "final-project-609a0.firebaseapp.com",
+  databaseURL: "https://final-project-609a0.firebaseio.com",
+  projectId: "final-project-609a0",
+  storageBucket: "final-project-609a0.appspot.com",
+  messagingSenderId: "381682991098",
+  appId: "1:381682991098:web:bee494d3d872cd5e5a2e43",
+  measurementId: "G-XN27NVRKTQ"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.use(NowUiKit);
 
