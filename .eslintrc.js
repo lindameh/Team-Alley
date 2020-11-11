90cb6476c47fc550a0fc2663d5621bce3c108b7a
Zi Ming <changziming@users.noreply.github.com>
2020-11-11 15:34:33 +0800

    set NODE_ENV=development

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,10 @@ module.exports = {
     node: true
   },
   extends: ['plugin:vue/essential'],
-
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+  },
   parserOptions: {
     parser: 'babel-eslint'
   }
